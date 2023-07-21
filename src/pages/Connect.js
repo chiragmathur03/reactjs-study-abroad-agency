6e8ebc9cddce842cd78e00743902db62d0823320
Chirag Mathur <54184768+chiragmathur03@users.noreply.github.com>
2023-07-22 00:51:35 +0530

    Fix Design

--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -3,7 +3,6 @@ import React from 'react'
 import { useState } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Switch } from '@headlessui/react'
-import { Link } from 'react-router-dom'
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(' ')
@@ -27,16 +26,22 @@ const Connect = () => {
                     }}
                 />
             </div>
-            <div className="mx-auto max-w-2xl text-center">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-                <p className="mt-2 text-lg leading-8 text-gray-600">
-                    Aute magna irure deserunt veniam aliqua magna enim voluptate.
+
+            {/* <div role="main" className="flex flex-col items-center justify-center">
+                <h1 className="text-4xl font-semibold tracking-tight text-center text-primary sm:text-5xl">News & Blogs</h1>
+                <p className="text-sm lg:text-base font-medium text-center text-gray-600 mt-4 lg:w-1/2 md:w-10/12 w-11/12">Stay updated with our news and blog for valuable insights and resources. Join our community and access the latest trends and opportunities in education.</p>
+            </div> */}
+
+            <div className="mx-auto max-w-2xl text-center mt-28 ">
+                <h2 className="text-3xl font-semibold tracking-tight text-primary sm:text-4xl">Contact Us</h2>
+                <p className="mt-2 text-sm lg:text-base font-medium text-gray-600">
+                    We are here to assist you on your study abroad journey.
                 </p>
             </div>
             <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
                 <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div>
-                        <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-600">
                             First name
                         </label>
                         <div className="mt-2.5">
@@ -45,12 +50,12 @@ const Connect = () => {
                                 name="first-name"
                                 id="first-name"
                                 autoComplete="given-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div>
-                        <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-600">
                             Last name
                         </label>
                         <div className="mt-2.5">
@@ -59,12 +64,12 @@ const Connect = () => {
                                 name="last-name"
                                 id="last-name"
                                 autoComplete="family-name"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="company" className="block text-sm font-semibold leading-6 text-gray-600">
                             Company
                         </label>
                         <div className="mt-2.5">
@@ -73,12 +78,12 @@ const Connect = () => {
                                 name="company"
                                 id="company"
                                 autoComplete="organization"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-600">
                             Email
                         </label>
                         <div className="mt-2.5">
@@ -87,12 +92,12 @@ const Connect = () => {
                                 name="email"
                                 id="email"
                                 autoComplete="email"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-600">
                             Phone number
                         </label>
                         <div className="relative mt-2.5">
@@ -106,7 +111,7 @@ const Connect = () => {
                                     className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                                 >
                                     <option>US</option>
-                                    <option>CA</option>
+                                    <option>IND</option>
                                     <option>EU</option>
                                 </select>
                                 <ChevronDownIcon
@@ -119,12 +124,12 @@ const Connect = () => {
                                 name="phone-number"
                                 id="phone-number"
                                 autoComplete="tel"
-                                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
                     </div>
                     <div className="sm:col-span-2">
-                        <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+                        <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-600">
                             Message
                         </label>
                         <div className="mt-2.5">
@@ -132,18 +137,18 @@ const Connect = () => {
                                 name="message"
                                 id="message"
                                 rows={4}
-                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                 defaultValue={''}
                             />
                         </div>
                     </div>
-                    <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+                    <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2 justify-center">
                         <div className="flex h-6 items-center">
                             <Switch
                                 checked={agreed}
                                 onChange={setAgreed}
                                 className={classNames(
-                                    agreed ? 'bg-indigo-600' : 'bg-gray-200',
+                                    agreed ? 'bg-primary' : 'bg-gray-200',
                                     'flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
                                 )}
                             >
@@ -158,18 +163,14 @@ const Connect = () => {
                             </Switch>
                         </div>
                         <Switch.Label className="text-sm leading-6 text-gray-600">
-                            By selecting this, you agree to our{' '}
-                            <Link to="#" className="font-semibold text-indigo-600">
-                                privacy&nbsp;policy
-                            </Link>
-                            .
+                            By selecting this, you agree to allow us to contact you via phone
                         </Switch.Label>
                     </Switch.Group>
                 </div>
                 <div className="mt-10">
                     <button
                         type="submit"
-                        className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        className="block w-full rounded-md bg-primary px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-primary-hover focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
                         Let's talk
                     </button>
