9bfeec58f3a1f521630b0e6b4e579a49469e52d2
onlyaiindia <onlyaiindia@gmail.com>
2024-12-25 11:48:55 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
         <div className="mx-auto lg:max-w-2xl">
           <div className="hidden md:mb-8 md:flex md:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Announcing our Latest Program for Medical Students.{' '}
+              Announcing ....{' '}
               <Link to="#" className="font-semibold text-primary">
                 <span className="absolute inset-0" aria-hidden="true" />
                 Read more <span aria-hidden="true">&rarr;</span>
